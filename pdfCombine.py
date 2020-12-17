14570de15edaefb2b78a826909e238fb55e899f1
Kris Shin <you@example.com>
2020-12-17 16:32:12 +0800

    comlete support jpg

--- pdfCombine.py
+++ pdfCombine.py
@@ -1,95 +1,81 @@
 import os
 import re
 from PyPDF4.pdf import PdfFileReader as pr, PdfFileWriter as pw
-from io import BytesIO
-from PIL import Image, ImageDraw
+import img2pdf
+import pikepdf._cpphelpers
 
 
-def MergePDF(dir_path):
-    pdf_files = list()
-    merged_file = pw()
+def main(dir_path):
+    file_list = get_file_list(dir_path)
+    merged_file = merge_pdf(file_list)
+    save_pdf(merged_file)
+    print('Done')
+    input()
+
+
+def conv_img2pdf(filename, ext):
+    with open(f'{filename}.pdf', 'wb') as pdf:
+        img = img2pdf.convert(filename+ext)
+        pdf.write(img)
+        filename += '.pdf'
+    return filename
+
+
+def get_file_list(dir_path):
+    file_list = list()
     for filename in os.listdir(dir_path):
-        ext = filename.split('.')[-1]
-        if filename in ['merged.pdf', 'tempImg.pdf']:
+        path, ext = os.path.splitext(filename)
+        if filename == 'merged.pdf':
             os.remove(filename)
             continue
-        if ext.lower() in ['pdf', 'jpg', 'jpeg', 'tiff']:
-            pdf_files.append(filename)
+        if ext in ['.jpg', '.pdf']:
+            if ext == '.jpg':
+                filename = conv_img2pdf(path, ext)
+            file_list.append(filename)
 
     # 按文件名中的数字排序
-    pdf_files = sorted(pdf_files, key=lambda i: int(re.findall(r'^(\d+).*?', i)
-                                     [0]) if re.findall(r'^(\d+).*?', i) else -1)
+    file_list = sorted(file_list, key=lambda i: int(re.findall(r'^(\d+).*?', i)
+                                                    [0]) if re.findall(r'^(\d+).*?', i) else -1)
+    return file_list
 
-    for pdf_file in pdf_files:
+
+def merge_pdf(file_list):
+    merged_file = pw()
+
+    for pdf_file in file_list:
         print(pdf_file)
-        ext = pdf_file.split('.')[-1]
-        if ext.lower() in ['jpg', 'png', 'jpeg', 'tiff']:
-            bimg = open(pdf_file, 'rb')
-            img = Image.open(BytesIO(bimg))
-            stream = BytesIO()
-            img.save(stream, ext)
-            img.close()
-            pg = pr().
+        # 读取PDF文件
+        try:
+            pdf = pr(open(pdf_file, "rb"))
+        except:
+            print(f'{pdf_file}无法解析')
+            continue
+        # 获得源PDF文件中页面总数
+        if pdf.isEncrypted:
+            print(f'{pdf_file} 是加密文件')
+            res = input('输入密码(回车键跳过)')
+            if res:
+                while res:
+                    try:
+                        pdf.decrypt(res)
+                    except:
+                        print('密码错误')
+                    res = input('输入密码(回车键跳过)')
+            continue
+        pageCount = pdf.getNumPages()
+        # 分别将page添加到输出output中
+        for iPage in range(pageCount):
+            merged_file.addPage(pdf.getPage(iPage))
+    return merged_file
 
-            for i in range(0, pdf.getNumPages()):
-                pg = pdf.getPage(i)
-                obj = pg['/Resources']['/XObject']
 
-                for j in obj:
-                    img = obj[j].getData()
-                    stream = covertImg(img)
-
-                    # 在PyPDF4 1.27.0中，此方法未实现，需要修改
-                    obj[j].setData(stream)
-
-                newpdf.addPage(pg)
-            
-            def covertImg(bimg):
-                img = Image.open(BytesIO(bimg))
-
-                # 看你想实现什么功能了
-                im = doSomething(img)
-                
-                stream = BytesIO()
-
-                # 将图片保存到stream中
-                # 注意，保存的格式要和pdf中原图片的格式保持一致
-                im.save(stream, 'jpeg')
-
-                img.close()
-                return stream.getvalue()
-        else:
-            # 读取PDF文件
-            try:
-                pdf = pr(open(pdf_file, "rb"))
-            except:
-                print(f'{pdf_file}无法解析')
-                continue
-            # 获得源PDF文件中页面总数
-            if pdf.isEncrypted:
-                print(f'{pdf_file} 是加密文件')
-                res = input('输入密码(回车键跳过)')
-                if res:
-                    while res:
-                        try:
-                            pdf.decrypt(res)
-                        except:
-                            print('密码错误')
-                        res = input('输入密码(回车键跳过)')
-                continue
-            pageCount = pdf.getNumPages()
-            # 分别将page添加到输出output中
-            for iPage in range(pageCount):
-                merged_file.addPage(pdf.getPage(iPage))
+def save_pdf(merged_file):
     with open('merged.pdf', "wb") as outputfile:
         # 注意这里的写法和正常的上下文文件写入是相反的
         merged_file.write(outputfile)
-    print('Done')
-
-    # input()
 
 
 if __name__ == '__main__':
     # 设置存放多个pdf文件的文件夹
     dir_path = r'.'
-    MergePDF(dir_path)
+    main(dir_path)
